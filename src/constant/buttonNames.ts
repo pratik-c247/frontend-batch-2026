5ff7be93903999342e6a4c60b0f31b6e7e88a8b4
Pratik Rathore <pratik.r@chapter247.com>
2026-04-11 15:27:20 +0530

    fix:improve auto-logout code

--- src/constant/buttonNames.ts
+++ src/constant/buttonNames.ts
@@ -1,4 +1,5 @@
-export const BUTTON_NAMES = { //Todo :- single file for button related constant
+export const BUTTON_NAMES = {
+  //Todo :- single file for button related constant
   CANCEL: 'Cancel',
   SUBMIT: 'Submit',
   RESEND_CODE: 'Resend code',
@@ -11,4 +12,7 @@ export const BUTTON_NAMES = { //Todo :- single file for button related constant
   SING_IN: 'Sign In',
   SIGN_UP: 'Sign Up',
   CREATE_ACCOUNT: 'Create Account',
+  LOG_OUT: 'Log Out',
+  LOG_OUT_NOW: 'Log Out Now',
+  STAY_LOGGED_IN: 'Stay Logged In',
 }
---
+++ src/features/dashboard.const.ts
@@ -0,0 +1,5 @@
+export const DASHBOARD_CONST = {
+  YOU_ARE_ON_DASHBOARD: 'You are on Dashboard',
+  YOUR_SESSION_WILL_AUTOMATICALLY:' Your session will automatically expire after 1 minute of inactivity.',
+  MY_APP:'MyApp'
+}
