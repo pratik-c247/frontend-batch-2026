7af252d117988766ae90f8aadeb2c27d9eeece51
Pratik Rathore <pratik.r@chapter247.com>
2026-04-23 13:29:21 +0530

    feat/initail commit

---
+++ src/data/fieldTypeOptions.ts
@@ -0,0 +1,89 @@
+// export const FIELD_TYPE_OPTIONS = [
+//   {
+//     label: 'Text',
+//     value: FIELD_TYPES.TEXT,
+//   },
+//   {
+//     label: 'Number',
+//     value: FIELD_TYPES.NUMBER,
+//   },
+//   {
+//     label: 'Date',
+//     value: FIELD_TYPES.DATE,
+//   },
+//   {
+//     label: 'Year',
+//     value: FIELD_TYPES.YEAR,
+//   },
+//   {
+//     label: 'Dropdown Select (Predefined)',
+//     value: FIELD_TYPES.PREDEFINED_DROPDOWN,
+//   },
+//   {
+//     label: 'Dropdown Select (Custom)',
+//     value: FIELD_TYPES.CUSTOM_DROPDOWN,
+//   },
+//   {
+//     label: 'Multi Select Dropdown',
+//     value: FIELD_TYPES.MULTI_SELECT_DROPDOWN,
+//   },
+//   {
+//     label: 'Textarea',
+//     value: FIELD_TYPES.TEXTAREA,
+//   },
+//   {
+//     label: 'Radio Button',
+//     value: FIELD_TYPES.RADIO_BUTTON,
+//   },
+//   {
+//     label: 'Checkbox',
+//     value: FIELD_TYPES.CHECKBOX,
+//   },
+//   {
+//     label: 'Toggle Switch',
+//     value: FIELD_TYPES.TOGGLE_SWITCH,
+//   },
+// ]
+
+ export const FIELD_TYPE_OPTIONS = [
+  {
+    label: 'Text',
+    value: 'text',
+  },
+  {
+    label: 'Number',
+    value: 'number',
+  },
+  {
+    label: 'Date',
+    value: 'date',
+  },
+  {
+    label: 'Year',
+    value: 'year',
+  },
+   {
+    label: 'Dropdown Select (Custom)',
+    value: 'custom_dropdown',
+  },
+  {
+    label: 'Multi Select Dropdown',
+    value: 'multi_select_dropdown',
+  },
+  {
+    label: 'Textarea',
+    value: 'textarea',
+  },
+  {
+    label: 'Radio Button',
+    value: 'radio_button',
+  },
+  {
+    label: 'Checkbox',
+    value: 'checkbox',
+  },
+  {
+    label: 'Toggle Switch',
+    value: 'toggle_switch',
+  },
+]
